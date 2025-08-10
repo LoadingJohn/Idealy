1e8af908b9ef2f2320127db4a1b29ec11fe8dc92
LoadingJohn <160551111+LoadingJohn@users.noreply.github.com>
2025-08-10 21:01:01 +1000

    Initial Commit

---
+++ Idealy/IdealyApp.swift
@@ -0,0 +1,21 @@
+//
+//  IdealyApp.swift
+//  Idealy
+//
+//  Created by John Underwood on 10/8/2025.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct IdealyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
